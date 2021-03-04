c8925eb0702bcbdd4ba758231dfc29fb7192d447
scott <scappworks@gmail.com>
2021-03-04 02:18:09 -0500

    seed flights into flight table. set root path in routes to be to flights index page. create directory 'app/views/layouts/flights'. add flights controller with index function.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Airport.create([{ airport_code: "NYC" }, { airport_code: "SFO" }])
+Flight.create([{ departure_airport_id: 1, destination_airport_id: 2, start_time: "2021-10-17 13:00", duration: 135 },
+    { departure_airport_id: 1, destination_airport_id: 2, start_time: "2021-02-11 10:30", duration: 135 },
+    { departure_airport_id: 2, destination_airport_id: 1, start_time: "2021-03-05 05:50", duration: 135 },
+    { departure_airport_id: 2, destination_airport_id: 1, start_time: "2021-08-28 04:45", duration: 135 }])
