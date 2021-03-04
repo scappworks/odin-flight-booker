6e9b77c34d7a661eb4737f3529713fd0a976805b
scott <scappworks@gmail.com>
2021-03-04 01:36:17 -0500

    add arriving and departing flights to airports and flights models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,4 @@
 class Flight < ApplicationRecord
+    belongs_to :departure_airport, class_name: "Airport", foreign_key: "departure_airport_id"
+    belongs_to :destination_airport, class_name: "Airport", foreign_key: "destination_airport_id"
 end
