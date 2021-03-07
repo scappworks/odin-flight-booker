c89145b4e882f901cf0fde4f2e2c1ebf6e3321ff
scott <scappworks@gmail.com>
2021-03-07 00:59:04 -0500

    add form for flight search result (currently does not work)

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,13 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+
+    @available_flights = Flight.where(departure_airport: params[:departure_airport], destination_airport: params[:destination_airport], 
+    flight_time: params[:flight_time], flight_date: params[:flight_date])
+  end
+
+  private
+  def flight_search_params
+    params.require(:flight).permit(:departure_airport_id, :destination_airport_id, :flight_time, :flight_date)
   end
 end
